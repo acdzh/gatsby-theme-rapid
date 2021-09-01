913959b693634154fb47a49f03487c517a12645e
acdzh <acdzh@outlook.com>
2021-09-01 15:51:17 +0800

    feat: update light code theme

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -4,7 +4,4 @@ import './src/style/normalize.css';
 import './src/style/theme.css';
 import './src/style/style.css';
 
-// Highlighting for code blocks
-// import 'prismjs/themes/prism.css';
-import 'prism-theme-one-light-dark/prism-onedark.css';
-import 'prismjs/plugins/line-numbers/prism-line-numbers.css';
+import './src/style/code-theme.scss';
